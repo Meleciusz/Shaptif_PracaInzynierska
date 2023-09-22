fab04115bdb0289f191c9f6157cea17befd368e2
Meleciusz <meleciusz@gmail.com>
2023-09-22 21:28:25 +0200

    adding cache file and authorization repository start

---
+++ packages/authorization_repository/lib/authorization_repository.dart
@@ -0,0 +1,2 @@
+export 'src/authorization_repository.dart';
+export 'src/models/models.dart';
---
+++ packages/cache/lib/cache.dart
@@ -0,0 +1,23 @@
+//Class is used to store cache(temporary data) data and to read it and write it by key.
+
+class CacheClient{
+  CacheClient() : _cache = <String, Object>{};
+
+  final Map<String, Object> _cache;
+
+  //Method that writes the provided [key], [data] pair to the memory cache.
+  void writeToMemoryCache<T extends Object>({required String key, required T data}){
+    _cache[key] = data;
+  }
+
+  //Method that reads the data from the memory cache by the provided key.
+  T? readFromMemoryCache<T extends Object>({required String key}){
+    final value = _cache[key];
+
+    if(value is T) {
+      return value;
+    } else {
+      return null;
+    }
+  }
+}
